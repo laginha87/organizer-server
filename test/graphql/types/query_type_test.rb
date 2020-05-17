3a3446d01676b29a91cbecc2696ff8c5242bf652
Filipe Correia <filipe.lp.correia@gmail.com>
2020-05-17 23:33:34 +0100

    test: adds 'tests' to query types

--- test/graphql/types/query_type_test.rb
+++ test/graphql/types/query_type_test.rb
@@ -2,8 +2,12 @@ require "test_helper"
 
 class Types::QueryTypeTest < ActiveSupport::TestCase
   test "it queries the inbox" do
-    skip "TODO query tests"
     query_string = read_gql("one")
+    inbox_result = ServerSchema.execute(query_string, context: {}, variables: {})
+  end
+
+  test "it queries projects" do
+    query_string = read_gql("two")
     inbox_result = ServerSchema.execute(query_string, context: {}, variables: {})
   end
 end
