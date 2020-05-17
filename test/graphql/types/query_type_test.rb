4c9ee5aea1ad17d387c17e12e6425dc8107b132e
Filipe Correia <filipe.lp.correia@gmail.com>
2020-05-17 23:36:35 +0100

    ci: fixes lint

--- test/graphql/types/query_type_test.rb
+++ test/graphql/types/query_type_test.rb
@@ -3,11 +3,11 @@ require "test_helper"
 class Types::QueryTypeTest < ActiveSupport::TestCase
   test "it queries the inbox" do
     query_string = read_gql("one")
-    inbox_result = ServerSchema.execute(query_string, context: {}, variables: {})
+    ServerSchema.execute(query_string, context: {}, variables: {})
   end
 
   test "it queries projects" do
     query_string = read_gql("two")
-    inbox_result = ServerSchema.execute(query_string, context: {}, variables: {})
+    ServerSchema.execute(query_string, context: {}, variables: {})
   end
 end
