971c9f5af8ed33b00e8cd89f3bddf67d76233462
Filipe Correia <filipe.lp.correia@gmail.com>
2020-05-22 23:16:35 +0100

    feat: adds default project timeslots

--- app/models/enums.rb
+++ app/models/enums.rb
@@ -3,4 +3,14 @@ module Enums
   DURATION = lambda { |name| {name => {short: "short", normal: "normal", long: "long"}, :_prefix => true} }
   DIFICULTY = lambda { |name| {name => {easy: "easy", medium: "medium", hard: "hard"}, :_prefix => true} }
   ICKYNESS = lambda { |name| {name => {chicken: "chicken", tepid: "tepid", frog: "frog"}, :_prefix => true} }
+
+  WEEKDAY = {
+    monday: "monday",
+    tuesday: "tuesday",
+    wednesday: "wednesday",
+    thursday: "thursday",
+    friday: "friday",
+    saturday: "saturday",
+    sunday: "sunday"
+  }
 end
--- test/graphql/types/query_type_test.rb
+++ test/graphql/types/query_type_test.rb
@@ -10,4 +10,20 @@ class Types::QueryTypeTest < ActiveSupport::TestCase
     query_string = read_gql("two")
     ServerSchema.execute(query_string, context: {}, variables: {})
   end
+
+  test "it queries " do
+    query_string = read_gql("three")
+    res = ServerSchema.execute(query_string, context: {}, variables: {})
+    data = res["data"]["defaultTimeslots"]
+
+    assert_equal("monday", data[0]["weekday"])
+    assert_equal("09:00", data[0]["startTime"])
+    assert_equal("18:00", data[0]["endTime"])
+    assert_equal("tuesday", data[1]["weekday"])
+    assert_equal("wednesday", data[2]["weekday"])
+    assert_equal("thursday", data[3]["weekday"])
+    assert_equal("friday", data[4]["weekday"])
+    assert_equal("saturday", data[5]["weekday"])
+    assert_equal("sunday", data[6]["weekday"])
+  end
 end
